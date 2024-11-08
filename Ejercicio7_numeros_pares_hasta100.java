1f8ecf3f38c3ab140d8140e0c2c744b9eb8e08b6
FerMendoza22 <lfayala@uth.hn>
2024-11-07 19:30:16 -0600

    Add files via upload

---
+++ Ejercicio7_numeros_pares_hasta100.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio7_numeros_pares_hasta100;
+
+/**
+ *
+ * @author Fernando Mendoza
+ */
+public class Ejercicio7_numeros_pares_hasta100 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 2; i <= 100; i +=2){
+        System.out.println(i);
+        }
+    }
+}
