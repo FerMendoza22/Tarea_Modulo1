30a7784540907313697654c034b5be17afda43f0
FerMendoza22 <lfayala@uth.hn>
2024-11-07 19:27:38 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._nombres;
+
+/**
+ *
+ * @author Fernando Mendoza
+ */
+public class App {
+
+
+    public static void main(String[] args) {
+        String[] nombres = new String[10];
+        
+        System.out.println ("ALUMNOS DE LA CLASE PROGRAMACION ORIENTADA A OBJETOS:");
+        nombres[0] = "1. ARNOLD ELIGIO AGUILAR CONTRERAS";
+        nombres[1] = "2. LISNY ALEXANDRA AGUILAR";
+        nombres[2] = "3. KEYSI YOELI AGUIRRE FUENTES";
+        nombres[3] = "4. ALEJANDRA MARIA LOPEZ RODRIGUEZ";
+        nombres[4] = "5. BRIPNY LIZETH DUARTE GOMEZ";
+        nombres[5] = "6. CARLOS ROBERTO QUINTERO CHICAS";
+        nombres[6] = "7. CRISTIAN FERNANDO ZAMORA ENAMORADO ";
+        nombres[7] = "8. DANLIS LEONEL BAIDE ENAMORADO";
+        nombres[8] = "9. DIMAS BELSASAR HERNANDEZ HERRERA";
+        nombres[9] = "10. EMANUEL ALEXANDER ORELLANA MARTINEZ";
+        
+        
+        for (int i= 0; i < nombres.length; i++ ){
+            System.out.println (nombres[i]);
+        }
+        
+       
+    }
+}
