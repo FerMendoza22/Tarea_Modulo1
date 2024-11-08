4c74d8fee4129fdc5b569dbd2e11cd60ad8285ec
FerMendoza22 <lfayala@uth.hn>
2024-11-07 19:31:34 -0600

    Add files via upload

---
+++ Hola_soy_Fernando_Mendoza.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hola_soy_fernando_mendoza;
+
+/**
+ *
+ * @author Fernando Mendoza
+ */
+public class Hola_soy_Fernando_Mendoza {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Luis Fernando Ayala Mendoza");
+    }
+}
